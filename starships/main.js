a53a644414d74d93158ea672810369df5f004875
Ari Curtis <you@example.com>
2020-11-12 10:57:00 -0700

    commit

--- starships/main.js
+++ starships/main.js
@@ -1,5 +1,5 @@
 import {starships} from '../data/starships.js'
-import {removeChildren, getLastNumber } from '../utils/index.js'
+import {removeChildren, getLastNumber, addStarField } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
@@ -51,3 +51,5 @@ function populateShipView(shipData){
 }
 
 populateNav(starships)
+
+addStarField(document.querySelector('body'), 1000)
