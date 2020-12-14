fd4e372dcd33dd380f2a96a6c8496c9eccb2d79e
Ari Curtis <you@example.com>
2020-12-14 01:55:00 -0700

    commit

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -1,58 +1,33 @@
-import{ films } from '../data/films (1)'
+import{ films } from '../data/films.js'
 import {getLastNumber} from '../utils/index.js'
 
 const main = document.querySelector('main')
 
-for (let step = 0; step < 7; step ++){
-let figure = document.createElement(' figure')
-let figImg = document.createElement('img')
-figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
-let figCaption = document.createElement('figcaption')
-
-const foundFilm = films.find(film => getLastNumber(film.url) == (step +1))
-figure.textContent = films[step].title
-
-figure.appendChild(figImg)
-figure.appendChild(figCaption)
-
-main.appendChild(figure)
-}
-
-
-//runs 7 times, with values of step 0-6
+for (let step = 0; step < 7; step++){
+    //as long as step is less than 7, incriment it by 1 
+    let figure = document.createElement('figure')
     let newImg = document.createElement('img') //new image instance
-    newImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg` //(step + 1)//set the source of it or nothing will show
+    newImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg` 
+    //set the source of it or nothing will show
+    //now append the image to the DOM somehow
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = films[step].title
+
+    figure.appendChild(figCaption)
+    figure.appendChild(newImg)
+    main.appendChild(figure)
+    }
+
+
+
+
+
+
+
+  /*  for (const film of films){
+    let newImg = document.createElement('img') //new image instance
+    newImg.src = 'https://starwars-visualguide.com/assets/img/films/2.jpg' //set the source of it or nothing will show
     //now append the image to the DOM somehow
     main.appendChild(newImg)
-    console.log(film.title);
-   // console.log (films[step]);
+    console.log(film.title);*/
 
-
-people.forEach(person => {
-    let newParagraph = document.body.appendChild(document.createElement('p'))
-    newParagraph.textContent = person.name
-})
-
-
-console.log(people.length)
-
-console.log(films[0])
-
-const film1 = document.querySelector('.filmlist')
-film1.textContent = films[0].title
-
-const film2 = document.querySelector('.filmlist')
-film1.textContent = films[1].title
-
-films.forEach(element => {
-    console.log(element.title)
-    filmList.textContent = element.title
-
-    for (const film of films){
-    let newImg = document.createElement('img') //new image instance
-    newImg.src = 'https://starwars-visualguide.com/assets/img/characters/2.jpg' //set the source of it or nothing will show
-    //now append the image to the DOM somehow
-    main.appendChild(newImg)
-    console.log(film.title);
-}
-}) 
